fdad6e01db802ada9940e9f9d88757d13ae26771
felip-araujo <felipearaujoexecutive@gmail.com>
2023-11-09 16:30:45 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+saldo = "";
+
+function verifSaldo(){
+   
+    var $result = document.querySelector('.result')
+    HTMLtemporario = $result.innerHTML, 
+    HTMLnovo = "O seu saldo é R$: " + saldo;  
+    HTMLtemporario = HTMLnovo + HTMLtemporario; 
+    $result.innerHTML = HTMLtemporario;
+
+} 
+
+function deposito(){
+    var novoValor = window.prompt("Qual o valor a depositar ?"); 
+    if(novoValor == null || novoValor == ""){
+        alert("Operação cancelada") 
+    } else{
+        var cash = novoValor; 
+        saldo =+ cash; 
+        alert("O saldo agora é " + saldo) 
+    }
+
+} 
+
+function saque(){
+    var novoSaque = window.prompt("Digite o valor que gostaria de sacar"); 
+        
+        if (novoSaque == null || novoSaque == "") {
+            alert("Operação Cancelada")
+        } else if(novoSaque > saldo){
+            alert("Saldo Insuficiente"); 
+        } else if(novoSaque <= saldo){
+            saldo = saldo - novoSaque; 
+            alert("O saldo agora é " + saldo); 
+        } else {
+            alert("Digite um valor válido")
+        }
+    
+}
